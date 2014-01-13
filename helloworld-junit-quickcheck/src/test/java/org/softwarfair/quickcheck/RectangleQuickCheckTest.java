2f3a3b19d5ae8fc5733772745a07f08f8ef833f5
robert <robertrv@gmail.com>
2014-01-14 00:53:21 +0100

    Moving things from old folders to new one

---
+++ helloworld-junit-quickcheck/src/test/java/org/softwarfair/quickcheck/RectangleQuickCheckTest.java
@@ -0,0 +1,55 @@
+package org.softwarfair.quickcheck;
+
+import net.java.quickcheck.QuickCheck;
+import net.java.quickcheck.characteristic.AbstractCharacteristic;
+import net.java.quickcheck.generator.PrimitiveGenerators;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class RectangleQuickCheckTest {
+    @Test
+    public void testArea() throws Exception {
+        for(int i = 1; i < 100; i++) {
+            Integer x = PrimitiveGenerators.positiveIntegers().next();
+            Integer y = PrimitiveGenerators.positiveIntegers().next();
+            Rectangle out = new Rectangle(x, y);
+            Assert.assertEquals(x*y, out.area());
+        }
+    }
+
+    @Test
+    public void testIsSquare() throws Exception {
+        for(int i = 1; i < 100; i++) {
+            Integer x = PrimitiveGenerators.positiveIntegers().next();
+            Rectangle out = new Rectangle(x, x);
+            Assert.assertTrue(out.isSquare());
+        }
+    }
+
+    @Test
+    public void testIsNotSquare() throws Exception {
+        for(int i = 1; i < 100; i++) {
+            Integer x = PrimitiveGenerators.positiveIntegers().next();
+
+            Integer y = PrimitiveGenerators.positiveIntegers().next();
+            while (x == y) {
+                y = PrimitiveGenerators.positiveIntegers().next();
+            }
+            Rectangle out = new Rectangle(x, y);
+            Assert.assertFalse(out.isSquare());
+        }
+    }
+
+    @Test
+    public void testIsSquareCharactestic() throws Exception {
+        QuickCheck.forAllVerbose(100, PrimitiveGenerators.positiveIntegers(), new AbstractCharacteristic<Integer>() {
+            @Override
+            protected void doSpecify(Integer x) throws Throwable {
+                Rectangle out = new Rectangle(x, x);
+                Assert.assertTrue(out.isSquare());
+            }
+        });
+
+
+    }
+}
